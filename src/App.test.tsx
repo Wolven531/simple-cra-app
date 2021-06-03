be6bbaab10492278d126dbc9326b09a9a4170aab
wolven531 <anthony.williams.cs@gmail.com>
2021-06-03 17:56:24 -0600

    Update unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+// import { render, screen } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+	let app: RenderResult
+
+	beforeEach(() => {
+		app = render(<App />)
+	})
+
+	it('renders app', () => {
+		expect(app).toBeDefined()
+
+		// const linkElement = screen.getByText(/learn react/i)
+		// expect(linkElement).toBeInTheDocument()
+	})
+})
