cdda98eee8cd58476b786905cdee49446c9f040c
wolven531 <anthony.williams.cs@gmail.com>
2021-06-03 18:28:58 -0600

    Use process.env.API_URL in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 
+const ENV_API_URL = process.env.API_URL
+
 function App() {
 	return (
 		<div className="">
@@ -8,6 +10,8 @@ function App() {
 				<h2>Simple CRA App</h2>
 			</header>
 			<p>Welcome to Anthony and Vinny's Simple CRA App™!</p>
+			<p>This web app is connected to the following API</p>
+			<input readOnly value={ENV_API_URL} />
 		</div>
 	)
 }
