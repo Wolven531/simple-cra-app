dc4920742e5b96c635979d6de777f41935426919
wolven531 <anthony.williams.cs@gmail.com>
2021-06-10 20:48:34 -0600

    Use Container to clean up source

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Grid } from '@material-ui/core'
+import { Button, Container, Grid } from '@material-ui/core'
 import { IconDemo } from './IconDemo'
 import './App.css'
 
@@ -7,15 +7,13 @@ const ENV_API_URL = process.env.REACT_APP_API_URL || '!!! ENV WAS MISSING URL !!
 
 function App() {
 	return (
-		<Grid container className="app">
-			<Grid item xs={12} className="header">
+		<Container maxWidth="sm" className="app">
+			<Container className="header">
 				<h2>Simple CRA App</h2>
-			</Grid>
-			<Grid item xs={12}>
+			</Container>
+			<Container>
 				<p>Welcome to Anthony and Vinny's Simple CRA App®!</p>
-			</Grid>
-			<Grid item xs={12}>
-				<Grid container alignItems="center">
+				<Grid container alignItems="center" justify="center" style={{ margin: '0 0 1rem 0' }}>
 					<Grid item>
 						API
 					</Grid>
@@ -26,16 +24,14 @@ function App() {
 						<Button color="primary" variant="outlined" style={{ }}>Test</Button>
 					</Grid>
 				</Grid>
-			</Grid>
-			<Grid item xs={12}>
 				<IconDemo/>
-			</Grid>
-			<Grid item xs={12} className="footer">
+			</Container>
+			<Container className="footer">
 				Anthony Williams, Vincent Leighton
 				<br/>
-				© 2021, Most rights reserved but available upon request
-			</Grid>
-		</Grid>
+				© 2021, Most rights reserved
+			</Container>
+		</Container>
 	)
 }
 
