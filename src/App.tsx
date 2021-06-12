d88a64b12274bac863ad02c2c33f7302c89af7bd
wolven531 <anthony.williams.cs@gmail.com>
2021-06-11 20:19:14 -0600

    Add token check button and logic

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,17 @@ function App() {
 			.then(text => { alert(text) })
 			.catch(console.error)
 	}
+	const pingTokenCheckEndpoint = () => {
+		fetch(
+			`${ENV_API_URL}/config/check-token`,
+			{
+				cache: 'no-cache',
+				method: 'get',
+			})
+			.then(resp => resp.text())
+			.then(text => { alert(!!text ? 'Valid' : 'NOT VALID') })
+			.catch(console.error)
+	}
 
 	return (
 		<Container maxWidth="sm" className="app">
@@ -32,10 +43,11 @@ function App() {
 					<Grid item style={{ margin: '0 1rem 0 1rem' }}>
 						<input disabled readOnly value={ENV_API_URL} />
 					</Grid>
-					<Grid item>
-						<Button color="primary" variant="contained" onClick={pingApiHealthEndpoint}>Test</Button>
-					</Grid>
 				</Grid>
+				<Container style={{ display: 'flex', justifyContent: 'center', }}>
+					<Button className="btn-health" color="primary" variant="contained" onClick={pingApiHealthEndpoint}>Test - Server Up?</Button>
+					<Button className="btn-token" color="secondary" variant="contained" onClick={pingTokenCheckEndpoint}>Test - Token valid?</Button>
+				</Container>
 				<IconDemo/>
 			</Container>
 			<Container className="footer">
