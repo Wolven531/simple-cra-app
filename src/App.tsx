3f8ad31e764e6a30ab6c803fe302f72132e382be
wolven531 <anthony.williams.cs@gmail.com>
2021-06-12 18:50:28 -0600

    Use new ApiService

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
 import React from 'react'
 import { Button, Container, Grid } from '@material-ui/core'
+import { ApiService } from './ApiService'
 // import { IconDemo } from './IconDemo'
 import './App.css'
 
 const ENV_API_URL = process.env.REACT_APP_API_URL || '!!! ENV WAS MISSING URL !!!'
 
 function App() {
-	const handleError = (err: Error) => {
-		console.error(err)
-		alert(`There was a problem -\n\n${err.message}`)
-	}
-	const pingApiHealthEndpoint = () => {
-		fetch(
-			`${ENV_API_URL}/health`,
-			{
-				cache: 'no-cache',
-				method: 'get',
-			})
-			.then(resp => resp.text())
-			.then(text => { alert(text) })
-			.catch(handleError)
-	}
-	const pingTokenCheckEndpoint = () => {
-		fetch(
-			`${ENV_API_URL}/config/check-token`,
-			{
-				cache: 'no-cache',
-				method: 'get',
-			})
-			.then(resp => resp.text())
-			.then(text => { alert(!!text ? 'Valid' : 'NOT VALID') })
-			.catch(handleError)
-	}
+	const api = new ApiService(ENV_API_URL)
 
 	return (
 		<Container maxWidth="sm" className="app">
@@ -49,8 +25,8 @@ function App() {
 					</Grid>
 				</Grid>
 				<Container style={{ display: 'flex', justifyContent: 'center', }}>
-					<Button className="btn-health" color="primary" variant="contained" onClick={pingApiHealthEndpoint}>Server Up?</Button>
-					<Button className="btn-token" color="secondary" variant="contained" onClick={pingTokenCheckEndpoint}>Token valid?</Button>
+					<Button className="btn-health" color="primary" variant="contained" onClick={() => { api.pingApiHealthEndpoint() }}>Server Up?</Button>
+					<Button className="btn-token" color="secondary" variant="contained" onClick={() => { api.pingTokenCheckEndpoint() }}>Token valid?</Button>
 				</Container>
 				{/* <IconDemo/> */}
 			</Container>
