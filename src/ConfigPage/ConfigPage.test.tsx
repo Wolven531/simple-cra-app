022ebd474e597df0c0a7678c69af629b97354baa
wolven531 <anthony.williams.cs@gmail.com>
2021-06-14 21:26:19 -0600

    Add assert for healthcheck button click

--- src/ConfigPage/ConfigPage.test.tsx
+++ src/ConfigPage/ConfigPage.test.tsx
@@ -1,10 +1,10 @@
-import { render, RenderResult } from '@testing-library/react'
+import { shallow, ShallowWrapper } from 'enzyme'
 import React from 'react'
 import { ApiService } from '../services/ApiService'
-import { ConfigPage } from './ConfigPage'
+import { ConfigPage, ConfigPageProps } from './ConfigPage'
 
 describe('ConfigPage component', () => {
-	let comp: RenderResult
+	let comp: ShallowWrapper<ConfigPageProps>
 	let mockApiService: ApiService
 
 	beforeEach(() => {
@@ -16,10 +16,27 @@ describe('ConfigPage component', () => {
 			pingTokenCheckEndpoint: jest.fn().mockResolvedValue(true),
 		} as unknown as ApiService
 
-		comp = render(<ConfigPage api={mockApiService} />)
+		comp = shallow(<ConfigPage api={mockApiService} />)
 	})
 
-	it('renders ConfigPage', () => {
+	it('renders ConfigPage component', () => {
 		expect(comp).toBeDefined()
+	})
+
+	describe('click health button', () => {
+		let buttonHealthcheck: ShallowWrapper
+
+		beforeEach(() => {
+			buttonHealthcheck = comp.find('.btn-health')
+
+			buttonHealthcheck.simulate('click')
+		})
+
+		it('renders button properly, invokes pingApiHealthEndpoint() on API w/ no params', () => {
+			expect(buttonHealthcheck.text()).toEqual('Server Up?')
+
+			expect(mockApiService.pingApiHealthEndpoint).toHaveBeenCalledTimes(1)
+			expect(mockApiService.pingApiHealthEndpoint).toHaveBeenLastCalledWith()
+		})
 	})
 })
