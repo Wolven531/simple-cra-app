031d312f70529b891a080a1307e52d90edf538f7
wolven531 <anthony.williams.cs@gmail.com>
2021-06-14 21:25:38 -0600

    Configure enzyme adapter

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// more info: https://github.com/testing-library/jest-dom
+// jest-dom adds custom jest matchers for asserts on DOM nodes, which allows things like
+//     expect(element).toHaveTextContent(/react/i)
+import '@testing-library/jest-dom'
+
+// Enzyme allows shallow rendering and interaction simulation (e.g. click)
+import Enzyme from 'enzyme'
+
+import ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated'
+// import * as ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated'
+// import ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated/build/ReactSeventeenAdapter'
+
+// !!! NOTE - ignore purported TS error below, tests work properly
+//     but imports appear odd; more info - https://www.npmjs.com/package/enzyme-adapter-react-17-updated
+Enzyme.configure({ adapter: new ReactSeventeenAdapter() })
