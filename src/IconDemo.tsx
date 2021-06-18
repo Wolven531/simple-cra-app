d4da089f201ca506a0ddb143b06f66d5a674f9ca
wolven531 <anthony.williams.cs@gmail.com>
2021-06-18 14:19:07 -0600

    Remove Grid from IconDemo

--- src/IconDemo.tsx
+++ src/IconDemo.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Grid from '@material-ui/core/Grid'
+import { Container } from '@material-ui/core'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import DeleteIcon from '@material-ui/icons/Delete'
-import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
-import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded'
-import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone'
-import DeleteSharpIcon from '@material-ui/icons/DeleteSharp'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined'
 import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded'
-import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone'
 import DeleteForeverSharpIcon from '@material-ui/icons/DeleteForeverSharp'
-import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation'
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded'
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp'
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone'
 import FourKIcon from '@material-ui/icons/FourK'
+import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation'
 import ThreeSixtyIcon from '@material-ui/icons/ThreeSixty'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import React from 'react'
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -28,52 +28,40 @@ function IconDemo() {
 	const classes = useStyles()
 
 	return (
-		<Grid container className={classes.root}>
-			<Grid item xs={4}>
-				<Typography>Filled</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<DeleteIcon />
-				<DeleteForeverIcon />
-			</Grid>
-			<Grid item xs={4}>
-				<Typography>Outlined</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<DeleteOutlinedIcon />
-				<DeleteForeverOutlinedIcon />
-			</Grid>
-			<Grid item xs={4}>
-				<Typography>Rounded</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<DeleteRoundedIcon />
-				<DeleteForeverRoundedIcon />
-			</Grid>
-			<Grid item xs={4}>
-				<Typography>Two Tone</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<DeleteTwoToneIcon />
-				<DeleteForeverTwoToneIcon />
-			</Grid>
-			<Grid item xs={4}>
-				<Typography>Sharp</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<DeleteSharpIcon />
-				<DeleteForeverSharpIcon />
-			</Grid>
-			<Grid item xs={4}>
-				<Typography>Edge-cases</Typography>
-			</Grid>
-			<Grid item xs={8}>
-				<ThreeDRotationIcon />
-				<FourKIcon />
-				<ThreeSixtyIcon />
-			</Grid>
-		</Grid>
+		<Container className={classes.root} style={{
+			alignItems: 'center',
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			// justifyItems: 'center',
+		}}>
+			<Typography >Filled</Typography>
+			<DeleteIcon />
+			<DeleteForeverIcon />
+			<br/>
+			<Typography>Outlined</Typography>
+			<DeleteOutlinedIcon />
+			<DeleteForeverOutlinedIcon />
+			<br/>
+			<Typography>Rounded</Typography>
+			<DeleteRoundedIcon />
+			<DeleteForeverRoundedIcon />
+			<br/>
+			<Typography>Two Tone</Typography>
+			<DeleteTwoToneIcon />
+			<DeleteForeverTwoToneIcon />
+			<br/>
+			<Typography>Sharp</Typography>
+			<DeleteSharpIcon />
+			<DeleteForeverSharpIcon />
+			<br/>
+			<Typography>Edge-cases</Typography>
+			<ThreeDRotationIcon />
+			<FourKIcon />
+			<ThreeSixtyIcon />
+		</Container>
 	)
 }
 
 export { IconDemo }
+
