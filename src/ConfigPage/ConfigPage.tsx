0001083f91b7c95bbabe9495711703f00480652e
wolven531 <anthony.williams.cs@gmail.com>
2021-06-17 20:11:41 -0600

    Add update token button and input field

--- src/ConfigPage/ConfigPage.tsx
+++ src/ConfigPage/ConfigPage.tsx
@@ -1,5 +1,5 @@
 import { Button, Container, Grid } from '@material-ui/core'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { ApiService } from '../services/ApiService'
 import './ConfigPage.css'
 
@@ -8,6 +8,9 @@ export interface ConfigPageProps {
 }
 
 const ConfigPage: FC<ConfigPageProps> = ({ api }) => {
+	const [newToken, setNewToken] = useState('')
+	const [secret, setSecret] = useState('')
+
 	const fireHealthCheck = async () => {
 		const msg = await api.pingApiHealthEndpoint()
 
@@ -20,7 +23,7 @@ const ConfigPage: FC<ConfigPageProps> = ({ api }) => {
 	}
 
 	return (
-		<Container>
+		<Container className="config-page">
 			<Grid container alignItems="center" justify="center" style={{ margin: '0 0 1rem 0' }}>
 				<Grid item>
 					API
@@ -47,6 +50,24 @@ const ConfigPage: FC<ConfigPageProps> = ({ api }) => {
 					Token valid?
 				</Button>
 			</Container>
+			<Grid container alignItems="center" justify="center" style={{ margin: '1rem 0' }}>
+				<Grid item>
+					<input value={newToken} onChange={e => { setNewToken(e.target.value) }} placeholder="New token" />
+				</Grid>
+				<Grid item style={{ margin: '0 1rem 0 1rem' }}>
+					<input value={secret} onChange={e => { setSecret(e.target.value) }} placeholder="Secret" />
+				</Grid>
+				<Grid item style={{ margin: '0 1rem 0 1rem' }}>
+					<Button
+						className="btn-update-token"
+						color="primary"
+						variant="contained"
+						onClick={() => {}}
+					>
+						Update Token
+					</Button>
+				</Grid>
+			</Grid>
 		</Container>
 	)
 }
