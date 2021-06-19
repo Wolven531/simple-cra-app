713af4f0c6092f1a66cf11f66712934c9062bca4
wolven531 <anthony.williams.cs@gmail.com>
2021-06-19 15:21:25 -0600

    Add unit tests for pingTokenUpdateEndpoint

--- src/services/ApiService.test.ts
+++ src/services/ApiService.test.ts
@@ -1,3 +1,4 @@
+import { HEADERS_JSON_CONTENT, HTTP_POST, NO_CACHE } from '../constants'
 import { ApiService } from './ApiService'
 
 describe('ApiService service', () => {
@@ -139,6 +140,60 @@ describe('ApiService service', () => {
 					.mockImplementationOnce(jest.fn())
 
 				resp = await service.pingTokenCheckEndpoint()
+			})
+
+			it('invokes handleError() properly and returns safe value', () => {
+				expect(resp).toEqual(false)
+				expect(mockFetch).toHaveBeenCalledTimes(1)
+				expect(spyHandleError).toHaveBeenCalledTimes(1)
+				expect(spyHandleError).toHaveBeenLastCalledWith(fakeError)
+			})
+		})
+
+		describe('pingTokenUpdateEndpoint() w/ mocked non-error response', () => {
+			const fakeResponseText = 'true'
+			const fakeSecret = 'some-secret'
+			const fakeToken = 'some-token'
+			let mockFetch: jest.SpyInstance
+			let resp: boolean
+
+			beforeEach(async () => {
+				mockFetch = jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+					text: jest.fn().mockResolvedValue(fakeResponseText),
+				} as unknown as Response)
+
+				resp = await service.pingTokenUpdateEndpoint(fakeSecret, fakeToken)
+			})
+
+			it('uses fetch() properly and returns processed value', () => {
+				expect(resp).toEqual(true)
+				expect(mockFetch).toHaveBeenCalledTimes(1)
+				expect(mockFetch).toHaveBeenLastCalledWith(
+					`${fakeUrl}/config/set-token`, {
+						cache: NO_CACHE,
+						method: HTTP_POST,
+						body: JSON.stringify({
+							secret: fakeSecret,
+							token: fakeToken,
+						}),
+						headers: HEADERS_JSON_CONTENT,
+					}
+				)
+			})
+		})
+
+		describe('pingTokenUpdateEndpoint() w/ mocked error response', () => {
+			const fakeError = 'fake-error'
+			let mockFetch: jest.SpyInstance
+			let resp: boolean
+			let spyHandleError: jest.SpyInstance
+
+			beforeEach(async () => {
+				mockFetch = jest.spyOn(window, 'fetch').mockRejectedValueOnce(fakeError)
+				spyHandleError = jest.spyOn<any, 'handleError'>(service, 'handleError')
+					.mockImplementationOnce(jest.fn())
+
+				resp = await service.pingTokenUpdateEndpoint('some-secret', 'some-token')
 			})
 
 			it('invokes handleError() properly and returns safe value', () => {
