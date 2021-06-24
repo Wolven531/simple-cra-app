7075dedd5d21a720ec0712c41d6581eebb06c4dc
wolven531 <anthony.williams.cs@gmail.com>
2021-06-23 19:04:23 -0600

    Style page not found text

--- src/PageNotFoundPage/PageNotFoundPage.tsx
+++ src/PageNotFoundPage/PageNotFoundPage.tsx
@@ -5,9 +5,9 @@ import './PageNotFoundPage.css'
 
 const PageNotFoundPage: FC = () => {
 	return (
-		<Grid container alignItems="center" justify="center">
+		<Grid container alignItems="center" justify="center" className="page-not-found">
 			<Grid item>
-				<Typography variant="h3">
+				<Typography variant="h5" align="center" gutterBottom>
 					Page not found. How did you even get here?
 				</Typography>
 			</Grid>
