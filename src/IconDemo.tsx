f3c92bb698fe4c7b54d2f824897a490c7efe57bc
Roughnick <JGStutson@gmail.com>
2021-06-27 22:25:12 -0600

    Updated Icon and PageNotFound to set Page name

--- src/IconDemo.tsx
+++ src/IconDemo.tsx
@@ -15,57 +15,70 @@ import FourKIcon from '@material-ui/icons/FourK'
 import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation'
 import ThreeSixtyIcon from '@material-ui/icons/ThreeSixty'
 import React from 'react'
+import { AppTitleContextConsumer } from './AppTitleContext'
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		root: {
 			color: theme.palette.text.primary,
 		},
-	}),
+	})
 )
 
 function IconDemo() {
 	const classes = useStyles()
 
 	return (
-		<Container className={classes.root} style={{
-			// alignItems: 'center',
-			// display: 'flex',
-			// flexDirection: 'column',
-			// justifyContent: 'center',
-			// justifyItems: 'center',
-		}}>
-			<Grid container alignItems="center" justify="center">
-				<Grid item>
-					<Typography >Filled</Typography>
-					<DeleteIcon />
-					<DeleteForeverIcon />
-					<br/>
-					<Typography>Outlined</Typography>
-					<DeleteOutlinedIcon />
-					<DeleteForeverOutlinedIcon />
-					<br/>
-					<Typography>Rounded</Typography>
-					<DeleteRoundedIcon />
-					<DeleteForeverRoundedIcon />
-					<br/>
-					<Typography>Two Tone</Typography>
-					<DeleteTwoToneIcon />
-					<DeleteForeverTwoToneIcon />
-					<br/>
-					<Typography>Sharp</Typography>
-					<DeleteSharpIcon />
-					<DeleteForeverSharpIcon />
-					<br/>
-					<Typography>Edge-cases</Typography>
-					<ThreeDRotationIcon />
-					<FourKIcon />
-					<ThreeSixtyIcon />
-				</Grid>
-			</Grid>
-		</Container>
+		<AppTitleContextConsumer>
+			{(context) => {
+				context.setTitle('Icon Demo Page')
+
+				return (
+					<Container
+						className={classes.root}
+						style={
+							{
+								// alignItems: 'center',
+								// display: 'flex',
+								// flexDirection: 'column',
+								// justifyContent: 'center',
+								// justifyItems: 'center',
+							}
+						}
+					>
+						<Grid container alignItems="center" justify="center">
+							<Grid item>
+								<Typography>Filled</Typography>
+								<DeleteIcon />
+								<DeleteForeverIcon />
+								<br />
+								<Typography>Outlined</Typography>
+								<DeleteOutlinedIcon />
+								<DeleteForeverOutlinedIcon />
+								<br />
+								<Typography>Rounded</Typography>
+								<DeleteRoundedIcon />
+								<DeleteForeverRoundedIcon />
+								<br />
+								<Typography>Two Tone</Typography>
+								<DeleteTwoToneIcon />
+								<DeleteForeverTwoToneIcon />
+								<br />
+								<Typography>Sharp</Typography>
+								<DeleteSharpIcon />
+								<DeleteForeverSharpIcon />
+								<br />
+								<Typography>Edge-cases</Typography>
+								<ThreeDRotationIcon />
+								<FourKIcon />
+								<ThreeSixtyIcon />
+							</Grid>
+						</Grid>
+					</Container>
+				)
+			}}
+		</AppTitleContextConsumer>
 	)
 }
 
 export { IconDemo }
-
--- src/PageNotFoundPage/PageNotFoundPage.tsx
+++ src/PageNotFoundPage/PageNotFoundPage.tsx
@@ -1,20 +1,43 @@
 import { Grid, Typography } from '@material-ui/core'
 import React, { FC } from 'react'
 import PageNotFoundImage from './PageNotFound.jpg'
+import { AppTitleContextConsumer } from '../AppTitleContext'
 import './PageNotFoundPage.css'
 
 const PageNotFoundPage: FC = () => {
 	return (
-		<Grid container alignItems="center" justify="center" className="page-not-found">
-			<Grid item>
-				<Typography variant="h5" align="center" color="error" gutterBottom>
-					Page not found. How did you even get here?
-				</Typography>
-			</Grid>
-			<img src={PageNotFoundImage} width="300" height="400" alt="pile of question marks" />
-		</Grid>
+		<AppTitleContextConsumer>
+			{(context) => {
+				context.setTitle('Page Not Found')
+
+				return (
+					<Grid
+						container
+						alignItems="center"
+						justify="center"
+						className="page-not-found"
+					>
+						<Grid item>
+							<Typography
+								variant="h5"
+								align="center"
+								color="error"
+								gutterBottom
+							>
+								Page not found. How did you even get here?
+							</Typography>
+						</Grid>
+						<img
+							src={PageNotFoundImage}
+							width="300"
+							height="400"
+							alt="pile of question marks"
+						/>
+					</Grid>
+				)
+			}}
+		</AppTitleContextConsumer>
 	)
 }
 
 export { PageNotFoundPage }
-
