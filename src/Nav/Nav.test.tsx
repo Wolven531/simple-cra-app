f42cda2c39f4a370e5a7dfd585f11fe8e6a7c8b8
wolven531 <anthony.williams.cs@gmail.com>
2021-06-27 18:46:39 -0600

    Clean up test imports

--- src/Nav/Nav.test.tsx
+++ src/Nav/Nav.test.tsx
@@ -1,6 +1,5 @@
 import { render, RenderResult } from '@testing-library/react'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import { Nav } from './Nav'
 
 describe('Nav component', () => {
