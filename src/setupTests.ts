63182ba8de460865400a3096af1bb2b6845c3e03
wolven531 <anthony.williams.cs@gmail.com>
2021-06-29 22:28:18 -0600

    Remove enzyme setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,15 +3,4 @@
 //     expect(element).toHaveTextContent(/react/i)
 import '@testing-library/jest-dom'
 
-// Enzyme allows shallow rendering and interaction simulation (e.g. click)
-import Enzyme from 'enzyme'
-
-import ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated'
-// import * as ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated'
-// import ReactSeventeenAdapter from 'enzyme-adapter-react-17-updated/build/ReactSeventeenAdapter'
-
-// !!! NOTE - ignore purported TS error below, tests work properly
-//     but imports appear odd; more info - https://www.npmjs.com/package/enzyme-adapter-react-17-updated
-Enzyme.configure({ adapter: new ReactSeventeenAdapter() })
-
 jest.spyOn(window, 'alert').mockImplementation(jest.fn())
