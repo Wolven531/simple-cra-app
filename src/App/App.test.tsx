a47536a86d7e6726491a1166657faf8f3ddba533
wolven531 <anthony.williams.cs@gmail.com>
2021-06-29 22:28:30 -0600

    Remove enzyme from App test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,12 +1,11 @@
 import { render, RenderResult } from '@testing-library/react'
-import { shallow, ShallowWrapper } from 'enzyme'
 import React from 'react'
 // import { ConfigPage } from '../ConfigPage/ConfigPage'
 import App from './App'
 
 describe('App component when process.env.REACT_APP_API_URL has value', () => {
 	const fakeApiUrl = 'http://fake-api.co'
-	let wrapper: ShallowWrapper
+	let comp: RenderResult
 	let origEnv: NodeJS.ProcessEnv
 
 	beforeEach(() => {
@@ -22,58 +21,42 @@ describe('App component when process.env.REACT_APP_API_URL has value', () => {
 			REACT_APP_API_URL: fakeApiUrl,
 		}
 
-		wrapper = shallow(<App />)
+		comp = render(<App />)
 	})
 
 	afterEach(() => {
 		process.env = origEnv
 	})
 
-	it('renders App, uses values from environment', () => {
-		expect(wrapper).toBeDefined()
-
-		// expect(wrapper.find(ConfigPage).props().api.apiUrl).toEqual(fakeApiUrl)
-		// const linkElement = screen.getByText(/learn react/i)
-		// expect(linkElement).toBeInTheDocument()
-	})
-
-	describe('mount component', () => {
-		let comp: RenderResult
-
-		beforeEach(() => {
-			comp = render(<App />)
-		})
-
-		it('renders App', () => {
-			expect(comp).toBeDefined()
-		})
+	it('renders mounted App', () => {
+		expect(comp).toBeDefined()
 	})
 })
 
-describe('App component when process.env.REACT_APP_API_URL is not set', () => {
-	let wrapper: ShallowWrapper
-	let origEnv: NodeJS.ProcessEnv
+// describe('App component when process.env.REACT_APP_API_URL is not set', () => {
+// 	let wrapper: ShallowWrapper
+// 	let origEnv: NodeJS.ProcessEnv
 
-	beforeEach(() => {
-		origEnv = process.env
+// 	beforeEach(() => {
+// 		origEnv = process.env
 
-		process.env = {
-			NODE_ENV: process.env.NODE_ENV,
-			PUBLIC_URL: process.env.PUBLIC_URL,
-			REACT_APP_API_URL: undefined,
-		}
+// 		process.env = {
+// 			NODE_ENV: process.env.NODE_ENV,
+// 			PUBLIC_URL: process.env.PUBLIC_URL,
+// 			REACT_APP_API_URL: undefined,
+// 		}
 
-		wrapper = shallow(<App />)
+// 		wrapper = shallow(<App />)
 
-	})
+// 	})
 
-	afterEach(() => {
-		process.env = origEnv
-	})
+// 	afterEach(() => {
+// 		process.env = origEnv
+// 	})
 
-	it('renders App, uses default value from App comp', () => {
-		expect(wrapper).toBeDefined()
+// 	it('renders App, uses default value from App comp', () => {
+// 		expect(wrapper).toBeDefined()
 
-		// expect(wrapper.find(ConfigPage).props().api.apiUrl).toEqual('!!! ENV WAS MISSING URL !!!')
-	})
-})
+// 		// expect(wrapper.find(ConfigPage).props().api.apiUrl).toEqual('!!! ENV WAS MISSING URL !!!')
+// 	})
+// })
