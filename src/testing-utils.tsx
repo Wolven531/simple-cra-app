2bbe2fb97c3d88304e02249f25ade124a3b32c16
wolven531 <anthony.williams.cs@gmail.com>
2021-07-05 22:16:55 -0600

    Add testing utils file

---
+++ src/testing-utils.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react'
+import { FC, useContext } from 'react'
+import { GlobalContext } from './GlobalContext'
+import { ApiService } from './services/ApiService'
+
+/**
+ * This method allows us to mock parts of the GlobalContext w/ ease
+ *
+ * @param functionalComp
+ * @param mockedContext
+ * @returns
+ */
+const renderCompWithMockedContext = (
+	functionalComp: FC,
+	mockedContext: any
+) => {
+	const MockedContextProvider: FC = () => {
+		const context = useContext(GlobalContext)
+
+		if (mockedContext.api) {
+			context.api = mockedContext.api as unknown as ApiService
+		}
+		if (mockedContext.setApi) {
+			context.setApi = mockedContext.setApi
+		}
+		if (mockedContext.title) {
+			context.title = mockedContext.title
+		}
+		if (mockedContext.setTitle) {
+			context.setTitle = mockedContext.setTitle
+		}
+
+		// render the Functional Component provided to us
+		return functionalComp({})
+	}
+
+	// render the context-enabled wrapper
+	return render(<MockedContextProvider />)
+}
+
+export { renderCompWithMockedContext }
