2b5236dd9916b52280a391552a01b946d7700656
wolven531 <anthony.williams.cs@gmail.com>
2021-07-05 22:12:45 -0600

    Add renderCompWithMockedContext()

--- src/ConfigPage/ConfigPage.test.tsx
+++ src/ConfigPage/ConfigPage.test.tsx
@@ -4,160 +4,179 @@ import { GlobalContext } from '../GlobalContext'
 import { ApiService } from '../services/ApiService'
 import { ConfigPage } from './ConfigPage'
 
+/**
+ * This method allows us to mock parts of the GlobalContext w/ ease
+ *
+ * @param functionalComp
+ * @param mockedContext
+ * @returns
+ */
+const renderCompWithMockedContext = (
+	functionalComp: FC,
+	mockedContext: any
+) => {
+	const MockedContextProvider: FC = () => {
+		const context = useContext(GlobalContext)
+		context.setTitle = mockedContext.setTitle
+		context.api = mockedContext.api as unknown as ApiService
+
+		// render the Functional Component provided to us
+		return functionalComp({})
+	}
+
+	// render the context-enabled wrapper
+	return render(<MockedContextProvider />)
+}
+
 describe('ConfigPage component', () => {
 	let comp: RenderResult
 	let mockSetTitle: jest.Mock
 
 	beforeEach(() => {
 		mockSetTitle = jest.fn()
-		// comp = render(<ConfigPage />)
-		const FakeWrapperComp: FC<any> = (props: any) => {
-			const context = useContext(GlobalContext)
-			context.setTitle = mockSetTitle
-			context.api = {
+
+		comp = renderCompWithMockedContext(ConfigPage, {
+			api: {
 				apiUrl: '',
-			} as unknown as ApiService
-
-			return <ConfigPage />
-		}
-
-		comp = render(<FakeWrapperComp />)
+			},
+			setTitle: mockSetTitle,
+		})
 	})
 
 	it('renders ConfigPage component', () => {
 		expect(comp).toBeDefined()
 	})
 
-// 	describe('click the health button while providing health check method', () => {
-// 		let mockHealthCheck: jest.Mock
+	// 	describe('click the health button while providing health check method', () => {
+	// 		let mockHealthCheck: jest.Mock
 
-// 		beforeEach(() => {
-// 			mockHealthCheck = jest.fn().mockResolvedValue(undefined)
+	// 		beforeEach(() => {
+	// 			mockHealthCheck = jest.fn().mockResolvedValue(undefined)
 
-// 			comp = render(
-// 				<ConfigPage defaultFireHealthCheck={mockHealthCheck} />
-// 			)
+	// 			comp = render(
+	// 				<ConfigPage defaultFireHealthCheck={mockHealthCheck} />
+	// 			)
 
-// 			const container = comp.container
+	// 			const container = comp.container
 
-// 			// method 1 - works
-// 			// const healthButton = container.querySelector('.btn-health')
-// 			// healthButton?.dispatchEvent(
-// 			// 	new MouseEvent('click', {
-// 			// 		bubbles: true,
-// 			// 		cancelable: true,
-// 			// 		view: window,
-// 			// 	})
-// 			// )
+	// 			// method 1 - works
+	// 			// const healthButton = container.querySelector('.btn-health')
+	// 			// healthButton?.dispatchEvent(
+	// 			// 	new MouseEvent('click', {
+	// 			// 		bubbles: true,
+	// 			// 		cancelable: true,
+	// 			// 		view: window,
+	// 			// 	})
+	// 			// )
 
-// 			// method 2 - works
-// 			const healthButton = container.querySelector('.btn-health')
-// 			fireEvent.click(healthButton as Element)
+	// 			// method 2 - works
+	// 			const healthButton = container.querySelector('.btn-health')
+	// 			fireEvent.click(healthButton as Element)
 
-// 			// method 3
-// 			// screen.getByRole('button', {
+	// 			// method 3
+	// 			// screen.getByRole('button', {
 
-// 			// }).querySelector
-// 		})
+	// 			// }).querySelector
+	// 		})
 
-// 		it('invokes provided fireHealthCheck()', () => {
-// 			expect(mockHealthCheck).toHaveBeenCalledTimes(1)
-// 			// expect(mockHealthCheck).toHaveBeenLastCalledWith()
-// 			// expect(healthButton?.textContent).toEqual('')
+	// 		it('invokes provided fireHealthCheck()', () => {
+	// 			expect(mockHealthCheck).toHaveBeenCalledTimes(1)
+	// 			// expect(mockHealthCheck).toHaveBeenLastCalledWith()
+	// 			// expect(healthButton?.textContent).toEqual('')
 
-// 			// const buttons = comp.getAllByRole('button', { })
+	// 			// const buttons = comp.getAllByRole('button', { })
 
-// 			// expect(buttons[0].classList.contains('btn-health')).toBeTruthy()
-// 			// expect(buttons[1].classList.contains('btn-token')).toBeTruthy()
-// 			// expect(buttons[2].classList.contains('btn-update-token')).toBeTruthy()
-// 		})
-// 	})
+	// 			// expect(buttons[0].classList.contains('btn-health')).toBeTruthy()
+	// 			// expect(buttons[1].classList.contains('btn-token')).toBeTruthy()
+	// 			// expect(buttons[2].classList.contains('btn-update-token')).toBeTruthy()
+	// 		})
+	// 	})
 
-// 	describe('click the health button w/o health check method', () => {
-// 		beforeEach(() => {
-// 			const container = comp.container
+	// 	describe('click the health button w/o health check method', () => {
+	// 		beforeEach(() => {
+	// 			const container = comp.container
 
-// 			// method 2 - works
-// 			const healthButton = container.querySelector('.btn-health')
-// 			fireEvent.click(healthButton as Element)
-// 		})
+	// 			// method 2 - works
+	// 			const healthButton = container.querySelector('.btn-health')
+	// 			fireEvent.click(healthButton as Element)
+	// 		})
 
-// 		it('invokes default fireHealthCheck()', () => {
-// 			// TODO - add code to test default fireHealthCheck more thoroughly
-// 			expect(true).toBeTruthy()
-// 		})
-// 	})
+	// 		it('invokes default fireHealthCheck()', () => {
+	// 			// TODO - add code to test default fireHealthCheck more thoroughly
+	// 			expect(true).toBeTruthy()
+	// 		})
+	// 	})
 
-// 	// describe('click check token button', () => {
-// 	// 	let buttonCheckToken: ShallowWrapper
+	// 	// describe('click check token button', () => {
+	// 	// 	let buttonCheckToken: ShallowWrapper
 
-// 	// 	beforeEach(() => {
-// 	// 		buttonCheckToken = comp.find('.btn-token')
+	// 	// 	beforeEach(() => {
+	// 	// 		buttonCheckToken = comp.find('.btn-token')
 
-// 	// 		buttonCheckToken.simulate('click')
-// 	// 	})
+	// 	// 		buttonCheckToken.simulate('click')
+	// 	// 	})
 
-// 	// 	it('renders button properly, invokes pingTokenCheckEndpoint() on API w/ no params', () => {
-// 	// 		expect(buttonCheckToken.text()).toEqual('Token valid?')
+	// 	// 	it('renders button properly, invokes pingTokenCheckEndpoint() on API w/ no params', () => {
+	// 	// 		expect(buttonCheckToken.text()).toEqual('Token valid?')
 
-// 	// 		expect(mockApiService.pingTokenCheckEndpoint).toHaveBeenCalledTimes(1)
-// 	// 		expect(mockApiService.pingTokenCheckEndpoint).toHaveBeenLastCalledWith()
+	// 	// 		expect(mockApiService.pingTokenCheckEndpoint).toHaveBeenCalledTimes(1)
+	// 	// 		expect(mockApiService.pingTokenCheckEndpoint).toHaveBeenLastCalledWith()
 
-// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
-// 	// 	})
-// 	// })
+	// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
+	// 	// 	})
+	// 	// })
 
-// 	// describe('click update token button w/o setting secret or token inputs', () => {
-// 	// 	let buttonUpdateToken: ShallowWrapper
+	// 	// describe('click update token button w/o setting secret or token inputs', () => {
+	// 	// 	let buttonUpdateToken: ShallowWrapper
 
-// 	// 	beforeEach(() => {
-// 	// 		buttonUpdateToken = comp.find('.btn-update-token')
+	// 	// 	beforeEach(() => {
+	// 	// 		buttonUpdateToken = comp.find('.btn-update-token')
 
-// 	// 		buttonUpdateToken.simulate('click')
-// 	// 	})
+	// 	// 		buttonUpdateToken.simulate('click')
+	// 	// 	})
 
-// 	// 	it('shows alert and does NOT invoke API', () => {
-// 	// 		expect(buttonUpdateToken.text()).toEqual('Update Token')
+	// 	// 	it('shows alert and does NOT invoke API', () => {
+	// 	// 		expect(buttonUpdateToken.text()).toEqual('Update Token')
 
-// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).not.toHaveBeenCalled()
+	// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).not.toHaveBeenCalled()
 
-// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
-// 	// 	})
-// 	// })
+	// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
+	// 	// 	})
+	// 	// })
 
-// 	// describe('click update token button when using provided secret and token props for inputs', () => {
-// 	// 	const fakeSecret = 'some-secret'
-// 	// 	const fakeToken = 'some-token'
-// 	// 	let buttonUpdateToken: ShallowWrapper
-// 	// 	// let inputSecret: ShallowWrapper
-// 	// 	// let inputToken: ShallowWrapper
+	// 	// describe('click update token button when using provided secret and token props for inputs', () => {
+	// 	// 	const fakeSecret = 'some-secret'
+	// 	// 	const fakeToken = 'some-token'
+	// 	// 	let buttonUpdateToken: ShallowWrapper
+	// 	// 	// let inputSecret: ShallowWrapper
+	// 	// 	// let inputToken: ShallowWrapper
 
-// 	// 	beforeEach(() => {
-// 	// 		comp = shallow(<ConfigPage api={mockApiService} defaultSecret={fakeSecret} defaultToken={fakeToken} />)
+	// 	// 	beforeEach(() => {
+	// 	// 		comp = shallow(<ConfigPage api={mockApiService} defaultSecret={fakeSecret} defaultToken={fakeToken} />)
 
-// 	// 		// const mockSetNewToken = jest.fn()
-// 	// 		// const mockSetSecret = jest.fn()
-// 	// 		// jest.spyOn(React, 'useState')
-// 	// 		// 	.mockImplementationOnce((defaultNewToken: string) => [defaultNewToken, mockSetNewToken])
-// 	// 		// 	.mockImplementationOnce((defaultSecret: string) => [defaultSecret, mockSetSecret])
+	// 	// 		// const mockSetNewToken = jest.fn()
+	// 	// 		// const mockSetSecret = jest.fn()
+	// 	// 		// jest.spyOn(React, 'useState')
+	// 	// 		// 	.mockImplementationOnce((defaultNewToken: string) => [defaultNewToken, mockSetNewToken])
+	// 	// 		// 	.mockImplementationOnce((defaultSecret: string) => [defaultSecret, mockSetSecret])
 
-// 	// 		buttonUpdateToken = comp.find('.btn-update-token')
-// 	// 		// inputSecret = comp.find('.input-secret')
-// 	// 		// inputToken = comp.find('.input-new-token')
+	// 	// 		buttonUpdateToken = comp.find('.btn-update-token')
+	// 	// 		// inputSecret = comp.find('.input-secret')
+	// 	// 		// inputToken = comp.find('.input-new-token')
 
-// 	// 		// inputSecret.simulate('change', { target: { value: fakeSecret } })
-// 	// 		// inputToken.simulate('change', { target: { value: fakeToken } })
+	// 	// 		// inputSecret.simulate('change', { target: { value: fakeSecret } })
+	// 	// 		// inputToken.simulate('change', { target: { value: fakeToken } })
 
-// 	// 		buttonUpdateToken.simulate('click')
-// 	// 	})
+	// 	// 		buttonUpdateToken.simulate('click')
+	// 	// 	})
 
-// 	// 	it('renders button properly, invokes pingTokenUpdateEndpoint() on API w/ proper params', () => {
-// 	// 		expect(buttonUpdateToken.text()).toEqual('Update Token')
+	// 	// 	it('renders button properly, invokes pingTokenUpdateEndpoint() on API w/ proper params', () => {
+	// 	// 		expect(buttonUpdateToken.text()).toEqual('Update Token')
 
-// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).toHaveBeenCalledTimes(1)
-// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).toHaveBeenCalledWith(fakeSecret, fakeToken)
+	// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).toHaveBeenCalledTimes(1)
+	// 	// 		expect(mockApiService.pingTokenUpdateEndpoint).toHaveBeenCalledWith(fakeSecret, fakeToken)
 
-// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
-// 	// 	})
-// 	// })
+	// 	// 		expect(window.alert).toHaveBeenCalledTimes(1)
+	// 	// 	})
+	// 	// })
 })
