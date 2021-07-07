85b55666a576267576063d42b8086da56900b543
wolven531 <anthony.williams.cs@gmail.com>
2021-07-06 18:52:17 -0600

    Add renderElemWithMockedContext

--- src/testing-utils.tsx
+++ src/testing-utils.tsx
@@ -38,4 +38,39 @@ const renderCompWithMockedContext = (
 	return render(<MockedContextProvider />)
 }
 
-export { renderCompWithMockedContext }
+/**
+ * This method allows us to mock parts of the GlobalContext w/ ease
+ *
+ * @param element
+ * @param mockedContext
+ * @returns
+ */
+const renderElemWithMockedContext = (
+	functionalComp: JSX.Element,
+	mockedContext: any
+) => {
+	const MockedContextProvider: FC = () => {
+		const context = useContext(GlobalContext)
+
+		if (mockedContext.api) {
+			context.api = mockedContext.api as unknown as ApiService
+		}
+		if (mockedContext.setApi) {
+			context.setApi = mockedContext.setApi
+		}
+		if (mockedContext.title) {
+			context.title = mockedContext.title
+		}
+		if (mockedContext.setTitle) {
+			context.setTitle = mockedContext.setTitle
+		}
+
+		// render the Element provided to us
+		return functionalComp
+	}
+
+	// render the context-enabled wrapper
+	return render(<MockedContextProvider />)
+}
+
+export { renderCompWithMockedContext, renderElemWithMockedContext }
