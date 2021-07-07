3d37f6841622f914f497c8e9bec1b55a5eced22b
wolven531 <anthony.williams.cs@gmail.com>
2021-07-06 19:04:44 -0600

    Add unit test to display open menu

--- src/Nav/Nav.test.tsx
+++ src/Nav/Nav.test.tsx
@@ -1,9 +1,10 @@
 import { RenderResult } from '@testing-library/react'
-import { renderElemWithMockedContext } from '../testing-utils'
-import { Nav } from './Nav'
+import userEvent from '@testing-library/user-event'
 import { createMemoryHistory, MemoryHistory } from 'history'
 import React from 'react'
 import { Router } from 'react-router-dom'
+import { renderElemWithMockedContext } from '../testing-utils'
+import { Nav } from './Nav'
 
 describe('Nav component', () => {
 	const fakeTitle = 'Some Page Title'
@@ -29,5 +30,30 @@ describe('Nav component', () => {
 
 	it('renders title appropriately', () => {
 		expect(comp.getByRole('heading')).toHaveTextContent(fakeTitle)
+	})
+
+	describe('click to open menu', () => {
+		const expectedLinkRoutes = ['/', '/config', '/icons', '/search-users']
+		const expectedLinkTexts = [
+			'Home',
+			'Config',
+			'Icon Demo',
+			'Search Users',
+		]
+
+		beforeEach(() => {
+			userEvent.click(comp.getByLabelText('menu'))
+		})
+
+		it('displays menu items', () => {
+			const links = comp.getAllByRole('link')
+
+			expect(links.map((elem) => elem.getAttribute('href'))).toEqual(
+				expectedLinkRoutes
+			)
+			expect(links.map((elem) => elem.textContent)).toEqual(
+				expectedLinkTexts
+			)
+		})
 	})
 })
