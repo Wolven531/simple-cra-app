344f1d4ee6ca4f0a19349c9f82694f9fe82d11b4
wolven531 <anthony.williams.cs@gmail.com>
2021-07-08 21:52:55 -0600

    Restyle footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import './Footer.css'
 
 function Footer() {
 	return (
-		<Container className="footer">
-			<Typography variant="body2" align="center" gutterBottom>
+		<Container className="footer" style={{ marginTop: '1rem' }}>
+			<Typography variant="body2" align="center" style={{ color: '#aaa' }} gutterBottom>
 				Anthony Williams, Vincent Leighton, Jonathan Stutson
 				<br />© 2021, Most rights reserved
 			</Typography>
