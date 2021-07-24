f8553de28a545a570d55162a2434ff0019c5b8e7
wolven531 <anthony.williams.cs@gmail.com>
2021-07-23 21:24:33 -0600

    Fix formatting and imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
+import '@fontsource/roboto'
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App/App'
+import { App } from './App/App'
+import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { theme } from './theme'
-
-import '@fontsource/roboto'
-import './index.css'
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -16,7 +15,7 @@ ReactDOM.render(
 			<App />
 		</ThemeProvider>
 	</React.StrictMode>,
-	document.getElementById('root'),
+	document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
