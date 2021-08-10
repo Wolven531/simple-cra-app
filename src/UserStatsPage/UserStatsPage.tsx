1a4fb3aaa292c561ec68e57616cec72b6cd782ab
Vincent Leighton <vincentleighton@gmail.com>
2021-08-09 18:38:25 -0700

    Updated results display for UserStatsPage Thought I did this before

--- src/UserStatsPage/UserStatsPage.tsx
+++ src/UserStatsPage/UserStatsPage.tsx
@@ -128,7 +128,7 @@ const UserStatsPage: FC = () => {
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{Object.entries(result).map(([key, value]) => (
+						{/* {Object.entries(result).map(([key, value]) => (
 							<TableRow key={key}>
 								<TableCell component="th" scope="row">
 									{key}
@@ -138,6 +138,134 @@ const UserStatsPage: FC = () => {
 								</TableCell>
 							</TableRow>
 						))}
+					</TableBody>
+				</Table> */}
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Average Kills</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.killsAvg}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Total Kills</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.killsTotal}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Average Deaths</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.deathsAvg}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Total Deaths</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.deathsTotal}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Average Assists</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.assistsAvg}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Total Assists</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.assistsTotal}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>KDA</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.kDA.toFixed(3)}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Gold Earned Average</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>
+									{result.goldEarnedAvg.toFixed(0)}
+								</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Gold Earned Total</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>
+									{result.goldEarnedTotal}
+								</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Average Time Played</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.timePlayedAvg}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Total Time Played</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>
+									{result.timePlayedTotal}
+								</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Game Count</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.gamesCount}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Wins</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.totalWins}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Losses</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.totalLosses}</Typography>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell component="th" scope="row">
+								<Typography>Win Percentage</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography>{result.winPercentage}%</Typography>
+							</TableCell>
+						</TableRow>
 					</TableBody>
 				</Table>
 			</Container>
