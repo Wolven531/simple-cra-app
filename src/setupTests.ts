5db6fc43575b80b015f0f4006dc84e6d03044d34
wolven531 <anthony.williams.cs@gmail.com>
2021-08-16 22:09:03 -0600

    Mock console functions and report on usage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,4 +3,19 @@
 //     expect(element).toHaveTextContent(/react/i)
 import '@testing-library/jest-dom'
 
-jest.spyOn(window, 'alert').mockImplementation(jest.fn())
+// mock window alert
+const mockAlert = jest.spyOn(window, 'alert').mockImplementation(jest.fn())
+
+// mock console methods
+const mockConsoleDebug = jest.spyOn(window.console, 'debug').mockImplementation(jest.fn())
+const mockConsoleError = jest.spyOn(window.console, 'error').mockImplementation(jest.fn())
+const mockConsoleInfo = jest.spyOn(window.console, 'info').mockImplementation(jest.fn())
+const mockConsoleLog = jest.spyOn(window.console, 'log').mockImplementation(jest.fn())
+const mockConsoleWarn = jest.spyOn(window.console, 'warn').mockImplementation(jest.fn())
+
+afterAll(() => {
+	jest.spyOn(window.console, 'log').mockRestore()
+
+	console.log(`console.log called ${mockConsoleLog.mock.calls.length} times`)
+	console.log(`window.alert called ${mockAlert.mock.calls.length} times`)
+})
